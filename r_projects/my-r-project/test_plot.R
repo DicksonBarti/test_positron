4217eb49d37a94093dbe8ffe4ade123b608acd8c
Dickson Barti <dbarti@bsp.com.pg>
2025-12-19 11:50:50 +1000

    Test #1

---
+++ r_projects/my-r-project/test_plot.R
@@ -0,0 +1,11 @@
+# Data
+values <- c(10, 20, 15, 25, 30)
+names(values) <- c("A", "B", "C", "D", "E")
+
+# Bar plot
+barplot(
+  values,
+  main = "Simple Bar Plot",
+  xlab = "Category",
+  ylab = "Value"
+)
